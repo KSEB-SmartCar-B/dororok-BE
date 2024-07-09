a4f87033ad3c7b0dcf315dd965cc366c492060d0
gyuseon <mn5592@naver.com>
2024-07-09 10:57:24 +0900

    스프링 부트 기본 설정

---
+++ src/main/java/com/smartcar/dororok/DororokApplication.java
@@ -0,0 +1,13 @@
+package com.smartcar.dororok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DororokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DororokApplication.class, args);
+	}
+
+}
