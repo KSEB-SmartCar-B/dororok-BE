559d51320ccd0ccb23aeae251eb1ddbbbf00edc3
gyuseon <mn5592@naver.com>
2024-07-11 14:51:48 +0900

    test : CI/CD 테스트 11

--- src/main/java/com/smartcar/dororok/spotify/controller/SpotifyController.java
+++ src/main/java/com/smartcar/dororok/spotify/controller/SpotifyController.java
@@ -23,7 +23,7 @@ public class SpotifyController {
 
     @GetMapping("/test")
     public String test(){
-        return "test 성공10";
+        return "test 성공11";
     }
 
     @GetMapping("/search")
