af7a99f3a1bde20f5553cd1fa8c8fcea6a40ec87
gyuseon <mn5592@naver.com>
2024-07-24 17:03:40 +0900

    feat : 장르 관련 설명 추가

--- src/main/java/com/smartcar/dororok/genre/controller/GenreController.java
+++ src/main/java/com/smartcar/dororok/genre/controller/GenreController.java
@@ -2,6 +2,8 @@ package com.smartcar.dororok.genre.controller;
 
 import com.smartcar.dororok.genre.repository.GenreRepository;
 import com.smartcar.dororok.genre.service.GenreService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,11 +15,13 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/genre")
+@Tag(name = "Genre Controller", description = "장르 관련 API")
 public class GenreController {
 
     private final GenreService genreService;
 
     @GetMapping("/name-list")
+    @Operation(summary = "모든 장르 이름 리스트", description = "모든 장르 이름 리스트 반환하는 API")
     public ResponseEntity<List<String>> nameList() {
         return ResponseEntity.ok(genreService.findAllGenreNames());
     }
