980c8287dbcc625302cbd5e355930533e0e71cf9
gyuseon <mn5592@naver.com>
2024-07-24 15:12:36 +0900

    refactor : MusicMode 컨버터 추가 및 적용

---
+++ src/main/java/com/smartcar/dororok/global/converter/StringToMusicModeConverter.java
@@ -0,0 +1,14 @@
+package com.smartcar.dororok.global.converter;
+
+import com.smartcar.dororok.recommendation.entity.MusicMode;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StringToMusicModeConverter implements Converter<String, MusicMode> {
+
+    @Override
+    public MusicMode convert(String source) {
+        return MusicMode.from(source);
+    }
+}
