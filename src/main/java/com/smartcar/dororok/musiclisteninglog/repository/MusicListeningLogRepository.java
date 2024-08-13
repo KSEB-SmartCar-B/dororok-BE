f1eb6522e99a699b02740df35918ba0b6e78ec0d
gyuseon <mn5592@naver.com>
2024-08-13 13:38:18 +0900

    refactor : 선호 장르 수정 로직 수정

--- src/main/java/com/smartcar/dororok/musiclisteninglog/repository/MusicListeningLogRepository.java
+++ src/main/java/com/smartcar/dororok/musiclisteninglog/repository/MusicListeningLogRepository.java
@@ -3,8 +3,16 @@ package com.smartcar.dororok.musiclisteninglog.repository;
 import com.smartcar.dororok.member.domain.entitiy.Member;
 import com.smartcar.dororok.musiclisteninglog.domain.MusicListeningLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
 
 public interface MusicListeningLogRepository extends JpaRepository<MusicListeningLog, Long> {
+
+    @Modifying
+    @Query("DELETE FROM MusicListeningLog m WHERE m.createdAt < :cutoffDate")
+    void deleteOlderThan(LocalDateTime cutoffDate);
 
     void deleteByMember(Member member);
 }
