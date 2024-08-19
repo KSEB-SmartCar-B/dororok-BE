32c28d2f04e12618c2a323a8d1d0b826b573d250
gyuseon <mn5592@naver.com>
2024-08-19 15:19:19 +0900

    fix : redis 설정 변경 (lettuce 대신 jedis 적용)

--- src/main/java/com/smartcar/dororok/global/config/RedisConfig.java
+++ src/main/java/com/smartcar/dororok/global/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -28,12 +29,13 @@ public class RedisConfig {
         redisConfiguration.setPort(Integer.parseInt(port));
         redisConfiguration.setPassword(password);
 
-        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
-                .commandTimeout(Duration.ofSeconds(10))  // 타임아웃을 10초로 설정
-                .build();
+//        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+//                .commandTimeout(Duration.ofSeconds(10))  // 타임아웃을 10초로 설정
+//                .build();
 
-        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisConfiguration, clientConfig);
-        return lettuceConnectionFactory;
+//        JedisConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisConfiguration, clientConfig);
+//        return lettuceConnectionFactory;
+        return new JedisConnectionFactory(redisConfiguration);
     }
 
     @Bean
